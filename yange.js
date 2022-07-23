cbe99599378a828ee2f4050ea2ecc47fa80f40d7
hassanyange <hassanyange@gmail.com>
2022-07-23 11:48:34 +0300

    first commit

---
+++ yange.js
@@ -0,0 +1,15 @@
+function validate () {    
+    var firstname =document.getElementById("fname").value;
+    var lastname =document.getElementById("lname").value;
+    var password =document.getElementById("password").value;
+    if (firstname == "hassan" && lastname == "yange" && password == "kivumbi"){
+        
+        //  alert("log in successfully");
+        window.location.href=("learn.html");
+       
+    }
+    else{
+        alert("invalid password or username");
+    }
+
+}
